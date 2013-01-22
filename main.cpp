b5a90699f0589866a1ca085fe2c24c1588449402
microcai <microcaicai@gmail.com>
2013-01-23 01:22:43 +0800

    check for clang

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #if defined _WIN32 || defined __CYGWIN__
 	#define SYMBOL_HIDDEN
 #else
-	#if __GNUC__ >= 4
+	#if __GNUC__ >= 4 ||  __clang__ >= 3
 	#define SYMBOL_HIDDEN  __attribute__ ((visibility ("hidden")))
 	#else
 	#define SYMBOL_HIDDEN
@@ -40,7 +40,7 @@ typedef boost::shared_ptr<avclient>	avclientptr;
 /**
  * 通过使用 SYMBOL_HIDDEN 让这个类不要导出，减少ELF文件体积.
  **/
-class SYMBOL_HIDDEN avclient :
+class avclient :
 	public boost::enable_shared_from_this<avclient>,
 	private boost::noncopyable
 {
@@ -52,19 +52,12 @@ public:
 		std::cout << "avclient deleted" << std::endl;
 	}
 private:
-	void typedetect(const boost::system::error_code & ec);
-	void handle_client_ssl_handshake(const boost::system::error_code & ec);
-	void handle_avserver_connected(const boost::system::error_code & ec){
-		if(!ec){
-			//执行 ssl handshake
-  			m_sslstream->async_handshake(ssl::stream_base::client,boost::bind(&avclient::handle_client_ssl_handshake,shared_from_this(),asio::placeholders::error));
-		}else{
-			std::cout << ec.message() <<std::endl;
-		}
-	}
-    void setup_ssl_cert();
+	void typedetect(const boost::system::error_code & ec) SYMBOL_HIDDEN;
+	void handle_ssl_handshake(const boost::system::error_code & ec) SYMBOL_HIDDEN;
+	void handle_avserver_connected(const boost::system::error_code & ec) SYMBOL_HIDDEN;
+    void setup_ssl_cert() SYMBOL_HIDDEN;
 private:
-	
+
 	enum {
 		AVCLIENT_TYPE_SOCKS5, //工作在client模式，意味着直接将数据SSL转发给远程的avsocks服务器.
 		AVCLIENT_TYPE_SERVER, //工作在server模式，意味着需要将SSL读取的数据直接转发给远程主机.
@@ -112,7 +105,7 @@ void avclient::typedetect(const boost::system::error_code& ec)
 
 		m_sslstream.reset(new ssl::stream<ip::tcp::socket&>(m_socket_server, m_sslctx));
 		//异步发起到 vps server的连接，并开始读取client的第一个请求，依据请求来判定是 socks5 还是 HTTP 还是透明代理.
-		m_socket_server.async_connect( m_avsocks_serveraddress,boost::bind(&avclient::handle_avserver_connected,shared_from_this(),asio::placeholders::error));
+		m_socket_server.async_connect(m_avsocks_serveraddress,boost::bind(&avclient::handle_avserver_connected,shared_from_this(),asio::placeholders::error));
 	}
 	//TODO 检查 HTTP 协议.
 	else{	//否则就是 ssl handshake 了.
@@ -124,15 +117,32 @@ void avclient::typedetect(const boost::system::error_code& ec)
 		m_sslstream.reset(new ssl::stream<ip::tcp::socket&>(*m_socket_client, m_sslctx));
 		// 执行 SSL 握手.
 		m_sslstream->async_handshake(ssl::stream_base::server,
-			boost::bind(&avclient::handle_client_ssl_handshake,shared_from_this(),asio::placeholders::error)
+			boost::bind(&avclient::handle_ssl_handshake,shared_from_this(),asio::placeholders::error)
 		);
 	}
 }
 
-void avclient::handle_client_ssl_handshake(const boost::system::error_code& ec)
+void avclient::handle_avserver_connected ( const boost::system::error_code& ec )
+{
+	if ( !ec ) {
+		//执行 ssl handshake
+		m_sslstream->async_handshake ( ssl::stream_base::client, boost::bind ( &avclient::handle_ssl_handshake, shared_from_this(), asio::placeholders::error ) );
+	} else {
+		std::cout << ec.message() << std::endl;
+	}
+}
+
+
+void avclient::handle_ssl_handshake(const boost::system::error_code& ec)
 {
 	if(!ec){
-		m_sslstream->write_some( asio::buffer("mabi",5) );//,[](const boost::system::error_code& ec){});
+		if(m_type == AVCLIENT_TYPE_SERVER){
+			//客户端已经被授权了，那么，开始处理吧，支持 SOCKS5 协议哦!
+		}else{
+			// splice过去, 协议的解析神码的都交给服务器来做就是了.
+			
+		}
+		//m_sslstream->write_some( asio::buffer("mabi",5) );//,[](const boost::system::error_code& ec){});
 	}else{
 		std::cout << ec.message() << std::endl;
 	}
