3d3472bab910767cc999bb93f8f6ca95ed890630
microcai <microcaicai@gmail.com>
2013-01-23 20:24:42 +0800

    macro some time is better code

--- splice.hpp
+++ splice.hpp
@@ -10,6 +10,12 @@
 
 namespace avsocks{
 
+#define ASIO_READ_PLACEHOLDERS asio::placeholders::error,asio::placeholders::bytes_transferred
+
+#define ASIO_WRITE_PLACEHOLDERS asio::placeholders::error,asio::placeholders::bytes_transferred
+
+#define BOOST_SHARED_THIS(classname) boost::enable_shared_from_this<classname<T,S1,S2> >::shared_from_this()
+
 template < class T , class S1, class S2>
 class splice : public boost::enable_shared_from_this<splice<T,S1,S2> >{
 public:
@@ -18,10 +24,10 @@ public:
 		:s1(_s1),s2(_s2),owner(_owner){}
 	void start(){
 		s1.async_read_some(s1s2buf.prepare(8192),
-			boost::bind(&splice<T,S1,S2>::s1s2_handle_read,boost::enable_shared_from_this<splice<T,S1,S2> >::shared_from_this(),asio::placeholders::error,asio::placeholders::bytes_transferred)
+			boost::bind(&splice<T,S1,S2>::s1s2_handle_read,BOOST_SHARED_THIS(splice),ASIO_READ_PLACEHOLDERS)
 		);
 		s2.async_read_some(s2s1buf.prepare(8192),
-			boost::bind(&splice<T,S1,S2>::s2s1_handle_read,boost::enable_shared_from_this<splice<T,S1,S2> >::shared_from_this(),asio::placeholders::error,asio::placeholders::bytes_transferred)
+			boost::bind(&splice<T,S1,S2>::s2s1_handle_read,BOOST_SHARED_THIS(splice),ASIO_READ_PLACEHOLDERS)
 		);
 	}
 	~splice(){
@@ -32,7 +38,7 @@ private:
 		if(!ec){
 			s1s2buf.commit(bytes_transferred);
 			s2.async_write_some(s1s2buf.data(),
-				boost::bind(&splice<T,S1,S2>::s1s2_handle_write,boost::enable_shared_from_this<splice<T,S1,S2> >::shared_from_this(),asio::placeholders::error,asio::placeholders::bytes_transferred)
+				boost::bind(&splice<T,S1,S2>::s1s2_handle_write,BOOST_SHARED_THIS(splice),ASIO_WRITE_PLACEHOLDERS)
 			);
 		}
 		else{
@@ -43,7 +49,7 @@ private:
 	void s1s2_handle_write(const boost::system::error_code & ec, std::size_t bytes_transferred){
 		if(!ec){
 			s1.async_read_some(s1s2buf.prepare(8192),
-				boost::bind(&splice<T,S1,S2>::s1s2_handle_read,boost::enable_shared_from_this<splice<T,S1,S2> >::shared_from_this(),asio::placeholders::error,asio::placeholders::bytes_transferred)
+				boost::bind(&splice<T,S1,S2>::s1s2_handle_read,BOOST_SHARED_THIS(splice),ASIO_READ_PLACEHOLDERS)
 			);
 		}else{
 			boost::system::error_code ec;
@@ -54,7 +60,7 @@ private:
 		if(!ec){
 			s2s1buf.commit(bytes_transferred);
 			s1.async_write_some(s2s1buf.data(),
-				boost::bind(&splice<T,S1,S2>::s2s1_handle_write,boost::enable_shared_from_this<splice<T,S1,S2> >::shared_from_this(),asio::placeholders::error,asio::placeholders::bytes_transferred)
+				boost::bind(&splice<T,S1,S2>::s2s1_handle_write,BOOST_SHARED_THIS(splice),ASIO_WRITE_PLACEHOLDERS)
 			);
 		}else{
 			boost::system::error_code ec;
@@ -64,7 +70,7 @@ private:
 	void s2s1_handle_write(const boost::system::error_code & ec, std::size_t bytes_transferred){
 		if(!ec){
 			s2.async_read_some(s2s1buf.prepare(8192),
-				boost::bind(&splice<T,S1,S2>::s2s1_handle_read,boost::enable_shared_from_this<splice<T,S1,S2> >::shared_from_this(),asio::placeholders::error,asio::placeholders::bytes_transferred)
+				boost::bind(&splice<T,S1,S2>::s2s1_handle_read,BOOST_SHARED_THIS(splice),ASIO_READ_PLACEHOLDERS)
 			);
 		}else{
 			boost::system::error_code ec;
@@ -77,5 +83,8 @@ private:
 	S2&						s2; //两个 socket
 	boost::shared_ptr<T>	owner;
 };
+
+#undef  ASIO_READ_PLACEHOLDERS
+#undef  BOOST_SHARED_THIS
 
 } // namespace avsocks.
