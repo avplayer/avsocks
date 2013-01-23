a71c8f9ba7228647dd1835d71104b87e3044f7a0
microcai <microcaicai@gmail.com>
2013-01-23 20:04:57 +0800

    add include for splice.hpp

--- splice.hpp
+++ splice.hpp
@@ -4,6 +4,10 @@
 
 #pragma once
 
+#include <boost/noncopyable.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
 namespace avsocks{
 
 template < class T , class S1, class S2>
