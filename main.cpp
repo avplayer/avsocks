b1f250bcba11bb02084f64b943feef0fdfe06cc6
microcai <microcaicai@gmail.com>
2013-01-24 00:32:15 +0800

    goto daemon

--- main.cpp
+++ main.cpp
@@ -175,6 +175,7 @@ static void do_accept(ip::tcp::acceptor &accepter,socketptr avsocketclient, cons
 
 int main(int argc, char **argv)
 {
+	bool dodaemon = false;
     std::string avserverport = "4567";
     std::string avserveraddress = "localhost";// = "avsocks.avplayer.org";//"fysj.com"
 
@@ -184,6 +185,7 @@ int main(int argc, char **argv)
 		( "help,h",													"produce help message" )
 		( "port,p",		po::value<std::string>(&avserverport),		"server port" )
 		( "avserver",	po::value<std::string>(&avserveraddress),	"avsocks server address" )
+		( "daemon,d",	po::value<bool>(&dodaemon),					"go into daemon mode" )
 		;
 
 	po::variables_map vm;
@@ -209,6 +211,8 @@ int main(int argc, char **argv)
 	{socketptr avsocketclient(new asio::ip::tcp::socket(accepter.get_io_service()));
 	accepter.async_accept(*avsocketclient,boost::bind(&do_accept,boost::ref(accepter),avsocketclient,asio::placeholders::error));}
 
+	if(dodaemon)
+		daemon(0,0);
 	return io_service.run()>0?0:1;
 }
 
