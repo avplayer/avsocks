2334404646d46f37e8dfe8bb00002df79604db20
microcai <microcaicai@gmail.com>
2013-01-25 17:44:32 +0800

    完成下载

--- gfwlist.hpp
+++ gfwlist.hpp
@@ -8,16 +8,20 @@
 #pragma once
 
 #include <ctime>
+#include <boost/bind.hpp>
 #include <boost/asio.hpp>
 namespace asio = boost::asio;
 #include <boost/filesystem.hpp>
 #include <boost/concept_check.hpp>
 namespace fs = boost::filesystem;
 
+#include <urdl/read_stream.hpp>
+#include <urdl/http.hpp>
+
 class gfwlist{
 public:
 	// 默认构造文件.
-	gfwlist(asio::io_service & _io_service):io_service(_io_service){
+	gfwlist(asio::io_service & _io_service):io_service(_io_service), m_urdl(io_service){
 #ifndef _WIN32
 		if(getenv("HOME"))
 			m_cached_gfwlist = fs::path(getenv("HOME")) / ".cache" / "gfwlist.txt";
@@ -50,10 +54,31 @@ public:
 		}
 		if(do_download){
 			// 下载文件吧，下载文件大丈夫~
+			
+				m_urdl.async_open( "", boost::bind( &gfwlist::googlecode_connected, this, asio::placeholders::error ) );
+			}
+	}
+
+	void googlecode_connected(const boost::system::error_code & ec){
+		if(!ec){
+			// 读取直到下载完成吧！
+			m_urdl.async_read_some( m_content.prepare ( 16384 ), boost::bind ( &gfwlist::handle_read, this, asio::placeholders::error, asio::placeholders::bytes_transferred ) );
 		}
 	}
 
+	void handle_read(const boost::system::error_code & ec, std::size_t readed){
+		if(!ec){
+			m_content.commit(readed);
+			m_urdl.async_read_some( m_content.prepare ( 16384 ), boost::bind ( &gfwlist::handle_read, this, asio::placeholders::error, asio::placeholders::bytes_transferred ) );
+		}else if(ec == asio::error::eof){
+			// download completed
+		}
+
+	}
+
 private:
-	asio::io_service & io_service;
-	fs::path		 m_cached_gfwlist;
+	asio::io_service&	io_service;
+	fs::path		 	m_cached_gfwlist;
+	urdl::read_stream	m_urdl;
+	asio::streambuf		m_content;
 };
