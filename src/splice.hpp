302564f27417f659b4da6267c1954e9b0ca63bbe
microcai <microcaicai@gmail.com>
2019-12-11 23:46:44 +0800

    update splice

--- src/splice.hpp
+++ src/splice.hpp
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <iostream>
 #include <boost/noncopyable.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
@@ -16,64 +17,77 @@ namespace avsocks{
 
 #define ASIO_WRITE_PLACEHOLDERS asio::placeholders::error,asio::placeholders::bytes_transferred
 
-template < class T , class S1, class S2>
+#define SPLICE_SIZE 1024*1024
+
+template <class T, class S1, class S2>
 class splice : public boost::enable_shared_from_this<splice<T,S1,S2> >{
 public:
 	using boost::enable_shared_from_this<splice<T,S1,S2> >::shared_from_this;
 	typedef boost::shared_ptr<splice>	pointer;
+
 	splice(boost::shared_ptr<T> _owner, S1& _s1, S2& _s2)
-		:s1(_s1),s2(_s2),owner(_owner){}
+		: s1(_s1),s2(_s2), owner(_owner) {}
+
 	void start(){
-		s1.async_read_some(s1s2buf.prepare(8192),
+		s1.async_read_some(boost::asio::buffer(s1s2buf),
 			boost::bind(&splice<T,S1,S2>::s1s2_handle_read,shared_from_this(),ASIO_READ_PLACEHOLDERS)
 		);
-		s2.async_read_some(s2s1buf.prepare(8192),
+		s2.async_read_some(boost::asio::buffer(s2s1buf),
 			boost::bind(&splice<T,S1,S2>::s2s1_handle_read,shared_from_this(),ASIO_READ_PLACEHOLDERS)
 		);
 	}
-	
-	~splice(){
 
+	~splice(){
 	}
+
 private:
 	void s1s2_handle_read(const boost::system::error_code & ec, std::size_t bytes_transferred){
 		if(!ec){
-			s1s2buf.commit(bytes_transferred);
-			s2.async_write_some(s1s2buf.data(),
+			boost::asio::async_write(s2, boost::asio::buffer(s1s2buf), boost::asio::transfer_exactly(bytes_transferred),
 				boost::bind(&splice<T,S1,S2>::s1s2_handle_write,shared_from_this(),ASIO_WRITE_PLACEHOLDERS)
 			);
 		}
-		else{
+		else if (ec == boost::asio::error::eof || bytes_transferred == 0){
 			boost::system::error_code ec;
+			s1.lowest_layer().close(ec);
 			s2.lowest_layer().shutdown(asio::socket_base::shutdown_both,ec);//->close();
+		}
+		else {
+			boost::system::error_code ec;
+			s1.lowest_layer().close(ec);
+			s2.lowest_layer().close(ec);
 		}
 	}
 	void s1s2_handle_write(const boost::system::error_code & ec, std::size_t bytes_transferred){
 		if(!ec){
-			s1s2buf.consume(bytes_transferred);
-			s1.async_read_some(s1s2buf.prepare(8192),
+			s1.async_read_some(boost::asio::buffer(s1s2buf),
 				boost::bind(&splice<T,S1,S2>::s1s2_handle_read,shared_from_this(),ASIO_READ_PLACEHOLDERS)
 			);
-		}else{
+		}
+		else {
 			boost::system::error_code ec;
-			s2.lowest_layer().shutdown(asio::socket_base::shutdown_both,ec);
+			s1.lowest_layer().close(ec);
+			s2.lowest_layer().close(ec);
 		}
 	}
 	void s2s1_handle_read(const boost::system::error_code & ec, std::size_t bytes_transferred){
 		if(!ec){
-			s2s1buf.commit(bytes_transferred);
-			s1.async_write_some(s2s1buf.data(),
+			boost::asio::async_write(s1, boost::asio::buffer(s2s1buf), boost::asio::transfer_exactly(bytes_transferred),
 				boost::bind(&splice<T,S1,S2>::s2s1_handle_write,shared_from_this(),ASIO_WRITE_PLACEHOLDERS)
 			);
+		}else if (ec == boost::asio::error::eof || bytes_transferred == 0){
+			boost::system::error_code ec;
+			s2.lowest_layer().close(ec);
+			s1.lowest_layer().shutdown(asio::socket_base::shutdown_both,ec);
 		}else{
 			boost::system::error_code ec;
-			s1.lowest_layer().shutdown(asio::socket_base::shutdown_both,ec);
+			s1.lowest_layer().close(ec);
+			s2.lowest_layer().close(ec);
 		}
 	}
 	void s2s1_handle_write(const boost::system::error_code & ec, std::size_t bytes_transferred){
 		if(!ec){
-			s2s1buf.consume(bytes_transferred);
-			s2.async_read_some(s2s1buf.prepare(8192),
+			s2.async_read_some(boost::asio::buffer(s2s1buf),
 				boost::bind(&splice<T,S1,S2>::s2s1_handle_read,shared_from_this(),ASIO_READ_PLACEHOLDERS)
 			);
 		}else{
@@ -82,10 +96,10 @@ private:
 		}
 	}
 private:
-	asio::streambuf	s1s2buf,s2s1buf;
-	S1&						s1; //两个 socket
-	S2&						s2; //两个 socket
-	boost::shared_ptr<T>	owner;
+	std::array<char, SPLICE_SIZE>	s1s2buf,s2s1buf;
+	S1&								s1; //两个 socket
+	S2&								s2; //两个 socket
+	boost::shared_ptr<T>		owner;
 };
 
 #undef  ASIO_READ_PLACEHOLDERS
